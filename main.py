a6b2c6f2cceeff0d09d3040b1b08cb19f8f593ec
franciscobmacedo <franciscovcbm@gmail.com>
2023-06-05 10:28:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import typer
+
+from pipeline import standardise
+
+
+def main(file: str, schema: str):
+    standardise(data_file_path=file, schema_file_path=schema)
+
+
+if __name__ == "__main__":
+    typer.run(main)
